711a68ce3c04777ff3409e2252747578bda95d12
Neeraj-eng <nirajnagar8085@gmail.com>
2026-04-12 13:09:39 +0530

    initial commit

---
+++ config/mongoconnect.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+export const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL,{
+      serverSelectionTimeoutMS: 5000
+    });
+    console.log("✅ MongoDB connected");
+  } catch (error) {
+    console.error("❌ MongoDB failed:", error.message);
+    throw error; // ✅ instead of process.exit
+  }
+};
